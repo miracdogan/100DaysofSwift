b043815d98a31a3fa2a3941aeeebd8f8d6bf6fd7
mriacdogan <miracdogn@gmail.com>
2024-06-05 00:02:30 +0300

    button functions and general design

--- GuessTheFlag/GuessTheFlag/ViewController.swift
+++ GuessTheFlag/GuessTheFlag/ViewController.swift
@@ -12,11 +12,58 @@ class ViewController: UIViewController {
     @IBOutlet var button2: UIButton!
     @IBOutlet var button3: UIButton!
     
+    var countries = [String]()
+    var score = 0
+    var correctAnswer = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco",
+                     "nigeria","poland","russia","spain","uk","us"]
+        
+        [button1, button2, button3].forEach {
+            $0?.imageView?.layer.borderWidth = 1
+            $0?.imageView?.layer.borderColor = UIColor.lightGray.cgColor
+        }
+        
+        askQuestion()
+        
+       
     }
 
-
+    func askQuestion(action: UIAlertAction! = nil) {
+        
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+        
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+        
+        title = countries[correctAnswer].uppercased()
+        
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(title: String(score), style: .plain, target: nil, action: nil)
+    }
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            score += 1
+        } else {
+            title = "Wrong"
+            score -= 1
+        }
+        
+        let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
+        
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        
+        present(ac, animated: true)
+        
+        
+    }
+    
 }
 
