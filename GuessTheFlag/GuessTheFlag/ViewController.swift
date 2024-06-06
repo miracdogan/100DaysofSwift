a5d4d7f10f547dd67bd3d4efabd8826d0a7cb468
mriacdogan <miracdogn@gmail.com>
2024-06-06 19:27:51 +0300

    Score and UIAlerts added.

--- GuessTheFlag/GuessTheFlag/ViewController.swift
+++ GuessTheFlag/GuessTheFlag/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
     var countries = [String]()
     var score = 0
     var correctAnswer = 0
-    
+    var numberOfQuestion = 1
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,8 +29,6 @@ class ViewController: UIViewController {
         }
         
         askQuestion()
-        
-       
     }
 
     func askQuestion(action: UIAlertAction! = nil) {
@@ -44,7 +42,10 @@ class ViewController: UIViewController {
         
         title = countries[correctAnswer].uppercased()
         
-        self.navigationItem.leftBarButtonItem = UIBarButtonItem(title: String(score), style: .plain, target: nil, action: nil)
+        let scoreLabel = UILabel()
+        scoreLabel.text = "Your score: \(score)"
+        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: scoreLabel)
+        
     }
     @IBAction func buttonTapped(_ sender: UIButton) {
         
@@ -53,17 +54,33 @@ class ViewController: UIViewController {
             score += 1
         } else {
             title = "Wrong"
+            let ac = UIAlertController(title: title, message: "Wrong! That's the flag of \(countries[sender.tag].capitalized)", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+            present(ac, animated: true)
             score -= 1
         }
         
-        let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
         
-        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        numberOfQuestion = numberOfQuestion + 1
         
-        present(ac, animated: true)
+        if numberOfQuestion == 11 {
+            let ac = UIAlertController(title: title, message: "You answered 10 questions and your final score is \(score).", preferredStyle: .alert)
+            
+            ac.addAction(UIAlertAction(title: "New Game", style: .default, handler: askQuestion))
+            
+            present(ac, animated: true)
+            
+            score = 0
+            numberOfQuestion = 0
+        } else {
+            let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
+            
+            ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+            
+            present(ac, animated: true)
+        }
         
         
     }
     
 }
-
