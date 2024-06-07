a629ca774cbbf5967aa9a3d1ffd299911981cacb
mriacdogan <miracdogn@gmail.com>
2024-06-07 12:46:32 +0300

    initial commit

---
+++ EasyBrowser/EasyBrowser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EasyBrowser
+//
+//  Created by Miraç Doğan on 7.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
