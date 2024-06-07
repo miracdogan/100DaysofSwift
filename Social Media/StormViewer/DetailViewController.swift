56635384dd54de36dae75906d881f728da2cc1b8
mriacdogan <miracdogn@gmail.com>
2024-06-07 12:44:46 +0300

    ActivityViewController Added

--- Social Media/StormViewer/DetailViewController.swift
+++ Social Media/StormViewer/DetailViewController.swift
@@ -22,6 +22,8 @@ class DetailViewController: UIViewController {
         }
         navigationItem.largeTitleDisplayMode = .never
 
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+        
         if let imageToLoad = selectedImage {
             imageView.image = UIImage(named: imageToLoad)
         }
@@ -36,6 +38,18 @@ class DetailViewController: UIViewController {
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         navigationController?.hidesBarsOnTap = false
+    }
+    
+    @objc func shareTapped() {
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8)
+        else {
+            print("No image found")
+            return
+        }
+        
+        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
     }
     
     /*
